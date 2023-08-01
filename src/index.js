0c265a923d086665ea70f4cf657319908830c3fc
Shantanu Gahtori <shantanugahtori4@gmail.com>
2023-08-02 03:24:04 +0530

    passing multiple instances

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Img ="https://m.media-amazon.com/images/I/51rBwNT0gEL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg";
-const Title = "American Prometheus";
-const Author = "Kai Bird";
+const firstBook = {
+Img: "https://m.media-amazon.com/images/I/51rBwNT0gEL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+Title: "American Prometheus",
+Author: "Kai Bird",
+}
+
+const secondBook = {
+Img: "https://m.media-amazon.com/images/I/51MUIvaD0XL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+Title: "Summer Sisters",
+Author: "Judy Blume",
+}
+
+
 
 
 function BookList(){
     return <section>
-        <Books img={Img} title={Title} author={Author} />
-        <Books img={Img} title={Title}/>
+        <Books img={firstBook.Img} title={firstBook.Title} author={firstBook.Author} />
+        <Books img={secondBook.Img} title={secondBook.Title}/>
     </section>;
 }
 
