b8de3db576c992414f988ce926488f17fad4f0ec
Shantanu Gahtori <shantanugahtori4@gmail.com>
2023-08-02 02:39:09 +0530

    nesting components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function FirstComponent(){
-    return <h2>First component</h2>;
+function BookList(){
+    return <section>
+        <Books />
+    </section>;
 }
+
+function Books(){
+    return <article>
+        <Image />
+        <Title />
+        <Author />
+    </article>
+}
+
+const Image = () => <img src="https://m.media-amazon.com/images/I/51rBwNT0gEL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" alt="" />;
+const Title = () => <h2>American Prometheus</h2>;
+const Author = () => <h2>Kai Bird</h2>;
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<FirstComponent/>);
+root.render(<BookList/>);
