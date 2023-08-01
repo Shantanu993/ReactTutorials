c4422076b32664070cc9cd98f7e7d4c86d7f2029
Shantanu Gahtori <shantanugahtori4@gmail.com>
2023-08-02 02:51:10 +0530

    adding js to jsx

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Img ="https://m.media-amazon.com/images/I/51rBwNT0gEL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg";
+const Title = "American Prometheus";
+const Author = "Kai Bird";
+
+
 function BookList(){
     return <section>
+        <Books />
         <Books />
     </section>;
 }
 
 function Books(){
     return <article>
-        <Image />
-        <Title />
-        <Author />
+        <img src={Img} alt="" />
+        <h2>{Title}</h2>
+        <h4>{Author}</h4>
     </article>
 }
 
-const Image = () => <img src="https://m.media-amazon.com/images/I/51rBwNT0gEL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" alt="" />;
-const Title = () => <h2>American Prometheus</h2>;
-const Author = () => <h2>Kai Bird</h2>;
 
 
 
